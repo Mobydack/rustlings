991439fb0a76ff7e90e12fb19197649f81ed94cd
Mobydack <devzetsubou@gmail.com>
2024-01-27 01:47:22 +0300

    complete: execises from 01 to 19 chapters

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples_count: i32) -> i32 {
+    let price = if apples_count > 40 { 1 } else { 2 };
+
+    apples_count * price
+}
 
 // Don't modify this function!
 #[test]
